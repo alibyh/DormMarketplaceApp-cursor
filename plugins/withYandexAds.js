6ce69e234c843ca9f3ef14a97865011f6e1b50d6
alibyh <alietud79@gmail.com>
2025-08-04 15:02:01 +0700

    isk

--- plugins/withYandexAds.js
+++ plugins/withYandexAds.js
@@ -1,54 +1,14 @@
-const { withDangerousMod } = require('@expo/config-plugins');
-const fs = require('fs');
-const path = require('path');
-
-const withYandexAds = (config) => {
-  return withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const xcodeProjectPath = path.join(config.modRequest.platformProjectRoot, 'DormMarketplaceApp');
-      
-      if (!fs.existsSync(xcodeProjectPath)) {
-        fs.mkdirSync(xcodeProjectPath, { recursive: true });
-      }
-
-      // Create the Objective-C files for Yandex Ads
-      const headerContent = `#import <React/RCTBridgeModule.h>
-
-@interface YandexAdsModule : NSObject <RCTBridgeModule>
-@end`;
-
-      const implementationContent = `#import "YandexAdsModule.h"
-@import YandexMobileAds;
-
-@implementation YandexAdsModule
-
-RCT_EXPORT_MODULE()
-
-RCT_EXPORT_METHOD(initializeSDK:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [YMAMobileAds initializeSDK];
-        resolve(nil);
-    });
-}
-
-@end`;
-
-      fs.writeFileSync(
-        path.join(xcodeProjectPath, 'YandexAdsModule.h'),
-        headerContent
+// plugins/withYandexAds.js
+module.exports = function withYandexAds(config, { sdkVersion = "7.13.0" } = {}) {
+  return require("@expo/config-plugins").withDangerousMod(config, [
+    "ios",
+    async (c) => {
+      const podLine = `  pod 'YandexMobileAds', '${sdkVersion}'`;
+      c.modResults.contents = c.modResults.contents.replace(
+        "use_expo_modules!",
+        `use_expo_modules!\n${podLine}`
       );
-
-      fs.writeFileSync(
-        path.join(xcodeProjectPath, 'YandexAdsModule.m'),
-        implementationContent
-      );
-
-      return config;
+      return c;
     },
   ]);
 };
-
-module.exports = withYandexAds;
